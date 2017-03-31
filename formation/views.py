2c0e8b1c132b9a627fed3e5126586a99e88e560b
Kevin Dunn <kgdunn@gmail.com>
2017-03-31 17:20:49 -0400

    Logging excessive informatino

--- formation/views.py
+++ formation/views.py
@@ -292,6 +292,7 @@ def index(request):
     """
     The main entry point
     """
+    logger.debug(str(request.POST))
     if development:
         # Creates a fake ``request`` that is used during development/debugging
         original_request = request
